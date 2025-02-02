95ca0c1fe459d5007a9f4ac4c052c8df51b101a9
JuanIsas <juan.isas@csuglobal.edu>
2025-02-01 20:54:08 -0800

    First check in

---
+++ Module 1/src/ModuleOne.java
@@ -0,0 +1,12 @@
+
+public class ModuleOne {
+
+	public static void main(String[] args) {
+		
+		System.out.println("First name : Juan");
+		System.out.println("Last name : Isas");
+		System.out.println("Address: 11111 Pomona Street, ");
+		System.out.println("Pomona");
+		System.out.println("Zip : 12345");	
+	}
+}
