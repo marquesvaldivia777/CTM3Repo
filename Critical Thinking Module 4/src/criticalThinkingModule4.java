243becd3d3aabc1c59a070336ce07de6acc9105d
JuanIsas <juan.isas@csuglobal.edu>
2025-02-03 16:04:29 -0800

    Initial Commit

---
+++ Critical Thinking Module 4/src/criticalThinkingModule4.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class criticalThinkingModule4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner scnr = new Scanner(System.in);
+		
+		double maxSoFar = 0;
+		double minSoFar = 0;
+		double currValue;
+		int i;
+		double valueSum = 0;
+		double nextValue; 
+		
+		System.out.println("Please provide 10 grades from the class: ");
+		
+		for (i=0; i < 10; i++) {
+			currValue = scnr.nextDouble();
+					
+			if (i==0) {
+				maxSoFar = currValue;				
+			}
+				else if (currValue > maxSoFar) {
+				maxSoFar = currValue;
+			}
+			
+			if (i==0) {
+				minSoFar = currValue;
+			}
+			else if (currValue < minSoFar) {
+				minSoFar = currValue;
+			}
+			
+			if (i==0) {
+				valueSum = currValue; 
+			}
+			else {valueSum = valueSum + currValue;}
+			
+			}
+		
+			double averageSum = valueSum/10;
+		
+		
+		System.out.println("Average: " + averageSum + "\n" + "Maximum: "
+		+ maxSoFar + "\n" + "Minimum: " + minSoFar);
+
+	}
+
+}
